37b239aef5728d00e08ec3ee723924bee7a94d88
bbbel11 <bbbelqa@gmail.com>
2025-05-18 15:25:10 +0300

    Удаление комментариев

--- qa1.py
+++ qa1.py
@@ -24,73 +24,72 @@ def browser():
 
 
 def test_practice_form(browser):
-    # Увеличиваем время ожидания элементов
+
     wait = WebDriverWait(browser, 20)
 
-    # 1. Открыть страницу Practice Form
+
     browser.get("https://demoqa.com/automation-practice-form")
 
-    # Ждем загрузки формы
+
     wait.until(EC.presence_of_element_located((By.ID, "firstName")))
 
-    # Генерация тестовых данных
+
     first_name = fake.first_name()
     last_name = fake.last_name()
     email = fake.email()
     mobile = fake.msisdn()[:10]
     current_address = fake.address().replace("\n", " ")
 
-    # 2. Заполнить форму
-    # Имя
+
     browser.find_element(By.ID, "firstName").send_keys(first_name)
 
-    # Фамилия
+
     browser.find_element(By.ID, "lastName").send_keys(last_name)
 
-    # Email
+
     browser.find_element(By.ID, "userEmail").send_keys(email)
 
-    # Пол (выбираем Male)
+
     male_radio = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//label[contains(text(),'Male')]")))
     male_radio.click()
 
-    # Номер телефона
+
     browser.find_element(By.ID, "userNumber").send_keys(mobile)
 
-    # Дата рождения
+
     dob_input = wait.until(EC.element_to_be_clickable((By.ID, "dateOfBirthInput")))
     dob_input.click()
 
-    # Выбираем месяц
+
     month_select = wait.until(EC.element_to_be_clickable(
         (By.CLASS_NAME, "react-datepicker__month-select")))
     month_select.send_keys("May")
 
-    # Выбираем год
+
     year_select = wait.until(EC.element_to_be_clickable(
         (By.CLASS_NAME, "react-datepicker__year-select")))
     year_select.send_keys("1990")
 
-    # Выбираем день
+
     day = wait.until(EC.element_to_be_clickable(
         (By.CLASS_NAME, "react-datepicker__day--015")))
     day.click()
 
-    # Предметы
+
     subjects_input = wait.until(EC.element_to_be_clickable((By.ID, "subjectsInput")))
     subjects_input.send_keys("Maths")
     subjects_input.send_keys(Keys.ENTER)
 
-    # Хобби
+
     sports_checkbox = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//label[contains(text(),'Sports')]")))
     sports_checkbox.click()
 
-    # Адрес
+
     browser.find_element(By.ID, "currentAddress").send_keys(current_address)
 
-    # Штат
+
     state_div = wait.until(EC.element_to_be_clickable((By.ID, "state")))
     browser.execute_script("arguments[0].scrollIntoView(true);", state_div)
     browser.execute_script("arguments[0].click();", state_div)
@@ -99,7 +98,7 @@ def test_practice_form(browser):
         (By.ID, "react-select-3-option-0")))
     browser.execute_script("arguments[0].click();", ncr_option)
 
-    # Город
+
     city_div = wait.until(EC.element_to_be_clickable((By.ID, "city")))
     browser.execute_script("arguments[0].click();", city_div)
 
@@ -107,18 +106,18 @@ def test_practice_form(browser):
         (By.ID, "react-select-4-option-0")))
     browser.execute_script("arguments[0].click();", delhi_option)
 
-    # Отправка формы
+
     submit_button = wait.until(EC.element_to_be_clickable((By.ID, "submit")))
     browser.execute_script("arguments[0].scrollIntoView(true);", submit_button)
     time.sleep(1)
     browser.execute_script("arguments[0].click();", submit_button)
 
-    # Проверка модального окна
+
     modal = wait.until(EC.visibility_of_element_located(
         (By.CLASS_NAME, "modal-content")))
     modal_text = modal.text
 
-    # Проверки
+
     assert f"Student Name {first_name} {last_name}" in modal_text
     assert f"Student Email {email}" in modal_text
     assert "Gender Male" in modal_text
@@ -129,7 +128,7 @@ def test_practice_form(browser):
     assert f"Address {current_address}" in modal_text
     assert "State and City NCR Delhi" in modal_text
 
-    # Закрытие модального окна
+
     close_button = wait.until(EC.element_to_be_clickable(
         (By.ID, "closeLargeModal")))
     close_button.click()
